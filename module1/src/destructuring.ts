349db0f8fe4ab8d4fee49710662e33e0633f6670
Coder Emran <coderemran6910@gmail.com>
2024-05-01 23:27:35 +0600

    Destructuring  with typeScript

---
+++ module1/src/destructuring.ts
@@ -0,0 +1,25 @@
+{
+
+    // Destructuring
+
+const friendsDetails = {
+    name: 'Md. Akramul Hoque',
+    age: 21, 
+    address: 'Dhaka',
+    education: 'Masters',
+    phone: '0170000000',
+    likes: {
+        food: 'Rice',
+        color: 'Blue',
+        car: 'BMW'
+    }
+}
+
+const {name, likes: {food: Food , color, car}} = friendsDetails
+
+
+
+
+
+
+}
