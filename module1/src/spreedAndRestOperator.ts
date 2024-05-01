c070bbebd0ac143872ca8aa5ee36f804e08d70b4
Coder Emran <coderemran6910@gmail.com>
2024-05-01 23:06:31 +0600

    learn spread and rest operator with typeScript

---
+++ module1/src/spreedAndRestOperator.ts
@@ -0,0 +1,25 @@
+{
+
+    // Spread Operator
+    // rest Operator
+    // destructuring 
+
+    
+// TypeScript in spread operator
+const friendList1: string[] = ['Rohim', 'Karim', 'Kamal', 'Nahid'];
+const friendList2: string[] = ['Jamal', 'Raihan', 'Faruk', 'Rabbil '];
+
+const friendList3 = friendList1.push(...friendList2);
+
+
+// typeScript in rest operator
+const friends = (...friends: string[]): string[] => {
+    return friends.forEach(element => {
+        
+    });
+}
+
+
+
+
+}
