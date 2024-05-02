8f4bafd447739ede210e43aeccaa5d5bbfcceae0
Coder Emran <coderemran6910@gmail.com>
2024-05-03 01:24:24 +0600

    added optional Chaining

---
+++ module1/src/TernsryOparator.ts
@@ -0,0 +1,22 @@
+{
+    const age = 18;
+
+    if (age >= 18) {
+        console.log('Adult');
+    } else {
+        console.log('Not Adult');
+    }
+
+
+
+
+    // Ternary Oparetor 
+
+ const isAdult = age>=18 ? 'adult': 'Not Adult'
+ console.log({isAdult});
+
+
+
+//  Nalish Coalescing Operator 
+ 
+}
